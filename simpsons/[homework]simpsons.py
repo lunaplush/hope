305f020f811eb76a8489abf79352d072658f04e3
lunaplush <lunaplus@mail.ru>
2023-04-14 19:59:02 +0300

    convolution and gradient 2

---
+++ simpsons/[homework]simpsons.py
@@ -0,0 +1,7 @@
+"""
+lunapluskaggle@gmail.com
+!Pridurki24
+user lunaplush
+ Александра_Максимова_54087465 - для отправки задания - прикрипления.
+"""
+kaggle competitions download -c journey-springfield
