a1d7a1c70ebe4daa89178f02d77a700d77edb626
lunaplush <lunaplus@mail.ru>
2023-04-17 20:12:40 +0300

    simpsons prepare

--- simpsons/[homework]simpsons.py
+++ simpsons/[homework]simpsons.py
@@ -4,4 +4,411 @@ lunapluskaggle@gmail.com
 user lunaplush
  Александра_Максимова_54087465 - для отправки задания - прикрипления.
 """
-kaggle competitions download -c journey-springfield
+import PIL
+import torch
+import numpy as np
+import pickle
+import numpy as np
+from skimage import io
+
+from tqdm import tqdm, tqdm_notebook
+from PIL import Image
+from pathlib import Path
+
+from torchvision import transforms
+from multiprocessing.pool import ThreadPool
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import f1_score
+from torch.utils.data import Dataset, DataLoader
+import torch.nn as nn
+
+from matplotlib import colors, pyplot as plt
+
+
+# в sklearn не все гладко, чтобы в colab удобно выводить картинки
+# мы будем игнорировать warnings
+import warnings
+warnings.filterwarnings(action='ignore', category=DeprecationWarning)
+
+
+from sklearn.model_selection import train_test_split
+
+# разные режимы датасета
+DATA_MODES = ['train', 'val', 'test']
+# все изображения будут масштабированы к размеру 224x224 px
+RESCALE_SIZE = 224
+# работаем на видеокарте
+#DEVICE = torch.device("cuda")
+DEVICE="cpu"
+
+
+class SimpsonsDataset(Dataset):
+    """
+    Датасет с картинками, который паралельно подгружает их из папок
+    производит скалирование и превращение в торчевые тензоры
+    """
+
+    def __init__(self, files, mode):
+        super().__init__()
+        # список файлов для загрузки
+        self.files = sorted(files)
+        # режим работы
+        self.mode = mode
+
+        if self.mode not in DATA_MODES:
+            print(f"{self.mode} is not correct; correct modes: {DATA_MODES}")
+            raise NameError
+
+        self.len_ = len(self.files)
+
+        self.label_encoder = LabelEncoder()
+
+        if self.mode != 'test':
+            self.labels = [path.parent.name for path in self.files]
+            self.label_encoder.fit(self.labels)
+
+            with open('label_encoder.pkl', 'wb') as le_dump_file:
+                pickle.dump(self.label_encoder, le_dump_file)
+
+    def __len__(self):
+        return self.len_
+
+    def load_sample(self, file):
+        image = Image.open(file)
+        image.load()
+        return image
+
+    def __getitem__(self, index):
+        # для преобразования изображений в тензоры PyTorch и нормализации входа
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+        x = self.load_sample(self.files[index])
+        x = self._prepare_sample(x)
+        x = np.array(x / 255, dtype='float32')
+        x = transform(x)
+        if self.mode == 'test':
+            return x
+        else:
+            label = self.labels[index]
+            label_id = self.label_encoder.transform([label])
+            y = label_id.item()
+            return x, y
+
+    def _prepare_sample(self, image):
+        image = image.resize((RESCALE_SIZE, RESCALE_SIZE))
+        return np.array(image)
+
+def imshow(inp, title=None, plt_ax=plt, default=False):
+    """Imshow для тензоров"""
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt_ax.imshow(inp)
+    if title is not None:
+        plt_ax.set_title(title)
+    plt_ax.grid(False)
+
+TRAIN_DIR = Path('train/simpsons_dataset')
+TEST_DIR = Path('testset/testset')
+
+train_val_files = sorted(list(TRAIN_DIR.rglob('*.jpg')))
+test_files = sorted(list(TEST_DIR.rglob('*.jpg')))
+
+
+train_val_labels = [path.parent.name for path in train_val_files]
+train_files, val_files = train_test_split(train_val_files, test_size=0.25, \
+                                          stratify=train_val_labels)
+## Изменила, чтобы обучать на всем множестве
+train_files = train_val_files
+
+val_dataset = SimpsonsDataset(val_files, mode='val')
+SAMPLES_PLOT = False
+if SAMPLES_PLOT:
+
+
+    fig, ax = plt.subplots(nrows=3, ncols=3,figsize=(8, 8), \
+                            sharey=True, sharex=True)
+    for fig_x in ax.flatten():
+        random_characters = int(np.random.uniform(0,1000))
+        im_val, label = val_dataset[random_characters]
+        img_label = " ".join(map(lambda x: x.capitalize(),\
+                    val_dataset.label_encoder.inverse_transform([label])[0].split('_')))
+        imshow(im_val.data.cpu(), \
+              title=img_label, plt_ax=fig_x)
+
+    plt.show()
+
+
+# Очень простая сеть
+class SimpleCnn(nn.Module):
+
+    def __init__(self, n_classes):
+        super().__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=8, kernel_size=3),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=96, kernel_size=3),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+
+        self.out = nn.Linear(96 * 5 * 5, n_classes)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+
+        x = x.view(x.size(0), -1)
+        logits = self.out(x)
+        return logits
+
+class MyCnn(nn.Module):
+
+    def __init__(self, n_classes):
+        super().__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=8, kernel_size=3),
+            nn.BatchNorm2d(8),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=96, kernel_size=3),
+            nn.BatchNorm2d(96),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.lin1 = nn.Sequential(
+            nn.Linear(96 * 5 * 5, 1000),
+            nn.ReLU(),
+        )
+
+        self.out = nn.Linear(1000, n_classes)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+
+
+        x = x.view(x.size(0), -1)
+        x = self.lin1(x)
+        logits = self.out(x)
+        return logits
+
+
+def fit_epoch(model, train_loader, criterion, optimizer):
+    running_loss = 0.0
+    running_corrects = 0
+    processed_data = 0
+
+    for inputs, labels in train_loader:
+        inputs = inputs.to(DEVICE)
+        labels = labels.to(DEVICE)
+        optimizer.zero_grad()
+
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        preds = torch.argmax(outputs, 1)
+        running_loss += loss.item() * inputs.size(0)
+        running_corrects += torch.sum(preds == labels.data)
+        processed_data += inputs.size(0)
+
+    train_loss = running_loss / processed_data
+    train_acc = running_corrects.cpu().numpy() / processed_data
+    return train_loss, train_acc
+
+
+def eval_epoch(model, val_loader, criterion):
+    model.eval()
+    running_loss = 0.0
+    running_corrects = 0
+    processed_size = 0
+
+    for inputs, labels in val_loader:
+        inputs = inputs.to(DEVICE)
+        labels = labels.to(DEVICE)
+
+        with torch.set_grad_enabled(False):
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            preds = torch.argmax(outputs, 1)
+
+        running_loss += loss.item() * inputs.size(0)
+        running_corrects += torch.sum(preds == labels.data)
+        processed_size += inputs.size(0)
+    val_loss = running_loss / processed_size
+    val_acc = running_corrects.double() / processed_size
+    return val_loss, val_acc
+
+
+def train(train_files, val_files, model, epochs, batch_size):
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+
+    history = []
+    log_template = "\nEpoch {ep:03d} train_loss: {t_loss:0.4f} \
+    val_loss {v_loss:0.4f} train_acc {t_acc:0.4f} val_acc {v_acc:0.4f}"
+
+    with tqdm(desc="epoch", total=epochs) as pbar_outer:
+        opt = torch.optim.Adam(model.parameters())
+        criterion = nn.CrossEntropyLoss()
+
+        for epoch in range(epochs):
+            train_loss, train_acc = fit_epoch(model, train_loader, criterion, opt)
+            print("loss", train_loss)
+
+            val_loss, val_acc = eval_epoch(model, val_loader, criterion)
+            history.append((train_loss, train_acc, val_loss, val_acc))
+
+            pbar_outer.update(1)
+            tqdm.write(log_template.format(ep=epoch + 1, t_loss=train_loss, v_loss=val_loss, t_acc=train_acc, v_acc=val_acc))
+
+    return history
+
+
+def predict(model, test_loader):
+    with torch.no_grad():
+        logits = []
+
+        for inputs in test_loader:
+            inputs = inputs.to(DEVICE)
+            model.eval()
+            outputs = model(inputs).cpu()
+            logits.append(outputs)
+
+    probs = nn.functional.softmax(torch.cat(logits), dim=-1).numpy()
+    return probs
+
+n_classes = len(np.unique(train_val_labels))
+simple_cnn = MyCnn(n_classes).to(DEVICE)
+print("we will classify :{}".format(n_classes))
+print(simple_cnn)
+
+if val_dataset is None:
+    val_dataset = SimpsonsDataset(val_files, mode='val')
+
+train_dataset = SimpsonsDataset(train_files, mode='train')
+
+TRAIN_STATUS = False
+if TRAIN_STATUS:
+    simple_cnn.load_state_dict(torch.load("my_cnn098.pth"))
+    history = train(train_dataset, val_dataset, model=simple_cnn, epochs=4, batch_size=512)
+    torch.save(simple_cnn.state_dict(), "my_cnn.pth")
+    loss, acc, val_loss, val_acc = zip(*history)
+    plt.figure(figsize=(15, 9))
+    plt.plot(loss, label="train_loss")
+    plt.plot(val_loss, label="val_loss")
+    plt.legend(loc='best')
+    plt.xlabel("epochs")
+    plt.ylabel("loss")
+    plt.show()
+else:
+    simple_cnn = MyCnn(n_classes).to(DEVICE)
+    #simple_cnn.load_state_dict(torch.load("simple_cnn.pth"))
+    simple_cnn.load_state_dict(torch.load("my_cnn.pth"))
+
+
+def predict_one_sample(model, inputs, device=DEVICE):
+    """Предсказание, для одной картинки"""
+    with torch.no_grad():
+        inputs = inputs.to(device)
+        model.eval()
+        logit = model(inputs).cpu()
+        probs = torch.nn.functional.softmax(logit, dim=-1).numpy()
+    return probs
+
+random_characters = int(np.random.uniform(0,1000))
+ex_img, true_label = val_dataset[random_characters]
+probs_im = predict_one_sample(simple_cnn, ex_img.unsqueeze(0))
+
+idxs = list(map(int, np.random.uniform(0,1000, 20)))
+idxs=[1,10,40,80,90,255,4000]
+imgs = [val_dataset[id][0].unsqueeze(0) for id in idxs]
+
+probs_ims = predict(simple_cnn, imgs)
+label_encoder = pickle.load(open("label_encoder.pkl", 'rb'))
+
+y_pred = np.argmax(probs_ims,-1)
+
+actual_labels = [val_dataset[id][1] for id in idxs]
+
+preds_class = [label_encoder.classes_[i] for i in y_pred]
+
+
+
+f1_scor = f1_score(actual_labels,y_pred, average='micro')
+print(list(zip(actual_labels, y_pred)))
+print(next(simple_cnn.parameters())[0][0])
+print(f1_scor)
+
+
+#------------------------------------------
+
+
+
+test_dataset = SimpsonsDataset(test_files, mode="test")
+
+test_loader = DataLoader(test_dataset, shuffle=False, batch_size=64)
+probs = predict(simple_cnn, test_loader)
+
+
+preds = label_encoder.inverse_transform(np.argmax(probs, axis=1))
+test_filenames = [path.name for path in test_dataset.files]
+
+
+
+
+import pandas as pd
+#my_submit = pd.read_csv("gdrive/My Drive/simpsons/data/labels.csv")
+my_submit = pd.DataFrame({'Id': test_filenames, 'Expected': preds})
+my_submit.head()
+
+my_submit.to_csv('mycnn.csv', index=False)
